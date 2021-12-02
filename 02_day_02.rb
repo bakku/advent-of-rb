f6aecbc160a5114ed07f6794a634d6488c24084f
Christian Paling <christian.paling@googlemail.com>
2021-12-02 09:41:40 +0100

    Add day 02

---
+++ 02_day_02.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+def evaluate_command(command, positions)
+  instruction, x = command.split(" ")
+
+  case instruction
+  when "forward"
+    positions.tap do |p|
+      p[:h] = p[:h] + x.to_i
+      p[:d] = p[:d] + p[:a] * x.to_i
+    end
+  when "down"
+    positions.tap { |p| p[:a] = p[:a] + x.to_i }
+  when "up"
+    positions.tap { |p| p[:a] = p[:a] - x.to_i }
+  end
+end
+
+commands = File.readlines("./02_input_01.txt", chomp: true)
+
+puts commands.each_with_object({ h: 0, d: 0, a: 0 }, &method(:evaluate_command))
+       .then { |positions| positions[:d] * positions[:h] }
