5869d15ce61e00239988e8738426da50079f96c0
Christian Paling <christian.paling@googlemail.com>
2021-12-03 14:09:19 +0100

    Add day 03

---
+++ 03_day/03_day_01.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+bit_numbers = File.readlines("./03_input_01.txt", chomp: true)
+initial_ary = Array.new(bit_numbers.first.size).map { [0, 0] }
+
+final_counts = bit_numbers.each_with_object(initial_ary) do |num, counts|
+  counts.tap do |c|
+    num.split("").each_with_index do |bit, i|
+      c[i][bit.to_i] += 1
+    end
+  end
+end
+
+gamma = final_counts.map { |c| c.index(c.max) }
+          .join("")
+          .to_i(2)
+
+epsilon = final_counts.map { |c| c.index(c.min) }
+            .join("")
+            .to_i(2)
+
+puts gamma * epsilon
---
+++ 03_day/03_day_02.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+def calc_bit_occurrence(bit_nums, pos)
+  bit_nums.each_with_object([0, 0]) do |num, counts|
+    counts.tap do |c|
+      c[num.chars[pos].to_i] += 1
+    end
+  end
+end
+
+def most_common_bit(bit_nums, pos)
+  counts = calc_bit_occurrence(bit_nums, pos)
+
+  return 1 if counts.max == counts.min
+
+  counts.index(counts.max)
+end
+
+def least_common_bit(bit_nums, pos)
+  counts = calc_bit_occurrence(bit_nums, pos)
+
+  return 0 if counts.max == counts.min
+
+  counts.index(counts.min)
+end
+
+def calc_rating(bit_numbers, pos, &block)
+  return bit_numbers.first.to_i(2) if bit_numbers.size == 1
+
+  mcb = block.call(bit_numbers, pos)
+
+  next_bit_numbers = bit_numbers.select { |bn| bn.chars[pos].to_i == mcb }
+
+  calc_rating(next_bit_numbers, pos + 1, &block)
+end
+
+def oxygen_rating(bit_numbers, pos) = calc_rating(bit_numbers, pos, &method(:most_common_bit))
+
+def co2_rating(bit_numbers, pos) = calc_rating(bit_numbers, pos, &method(:least_common_bit))
+
+bit_numbers = File.readlines("./03_input_02.txt", chomp: true)
+
+puts oxygen_rating(bit_numbers, 0) * co2_rating(bit_numbers, 0)
