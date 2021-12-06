82d6ba7f5321593a4b77f99b0ebf181eb1a38e83
Christian Paling <christian.paling@googlemail.com>
2021-12-06 14:29:01 +0100

    Add day 05 in kotlin

---
+++ 05_day/05_day_01.kt
@@ -0,0 +1,35 @@
+import java.io.File
+
+data class Line(val startX: Int, val startY: Int, val endX: Int, val endY: Int)
+
+fun List<List<Int>>.toLine(): Line {
+    val x1 = this[0][0]
+    val y1 = this[0][1]
+    val x2 = this[1][0]
+    val y2 = this[1][1]
+
+    return if (x1 == x2) Line(x1, minOf(y1, y2), x2, maxOf(y1, y2))
+    else Line(minOf(x1, x2), y1, maxOf(x1, x2), y2)
+}
+
+fun initMatrix(lines: List<Line>): Array<Array<Int>> {
+    val maxX = lines.maxOf { it.endX }
+    val maxY = lines.maxOf { it.endY }
+
+    return Array(maxX + 1) { Array(maxY + 1) { 0 } }
+}
+
+val lines = File("./05_input_01.txt")
+    .readLines()
+    .map { outer -> outer.split(" -> ").map { it.split(",").map(String::toInt) } }
+    .filter { it[0][0] == it[1][0] || it[0][1] == it[1][1] }
+    .map { it.toLine() }
+
+val resultingMatrix = lines.fold(initMatrix(lines)) { matrix, line ->
+    matrix.apply {
+        if (line.startX == line.endX) (line.startY..line.endY).forEach { this[line.startX][it] += 1 }
+        else (line.startX..line.endX).forEach { this[it][line.startY] += 1}
+    }
+}
+
+println(resultingMatrix.flatten().count { it > 1 })
---
+++ 05_day/05_day_02.kt
@@ -0,0 +1,79 @@
+import java.io.File
+
+interface Line {
+    val startX: Int
+    val startY: Int
+    val endX: Int
+    val endY: Int
+
+    fun construct(): List<Pair<Int, Int>>
+}
+
+class VerticalLine(override val startX: Int, override val startY: Int,
+                   override val endX: Int, override val endY: Int) : Line {
+    override fun construct(): List<Pair<Int, Int>> {
+        return if (startY < endY) (startY..endY).map { Pair(startX, it) }
+        else (endY..startY).map { Pair(startX, it) }
+    }
+}
+
+class HorizontalLine(override val startX: Int, override val startY: Int,
+                     override val endX: Int, override val endY: Int) : Line {
+    override fun construct(): List<Pair<Int, Int>> {
+        return if (startX < endX) (startX..endX).map { Pair(it, startY) }
+        else (endX..startX).map { Pair(it, startY) }
+    }
+}
+
+class DiagonalLine(override val startX: Int, override val startY: Int,
+                   override val endX: Int, override val endY: Int) : Line {
+    override fun construct(): List<Pair<Int, Int>> {
+        val xStep = if (startX < endX) 1 else -1
+        val yStep = if (startY < endY) 1 else -1
+        val pairList = mutableListOf<Pair<Int, Int>>()
+
+        var currentX: Int = startX
+        var currentY: Int = startY
+
+        while (currentX != endX) {
+            pairList.add(Pair(currentX, currentY))
+            currentX += xStep
+            currentY += yStep
+        }
+
+        pairList.add(Pair(currentX, currentY))
+
+        return pairList
+    }
+}
+
+fun List<List<Int>>.toLine(): Line {
+    val x1 = this[0][0]
+    val y1 = this[0][1]
+    val x2 = this[1][0]
+    val y2 = this[1][1]
+
+    return if (x1 == x2) VerticalLine(x1, y1, x2, y2)
+    else if (y1 == y2) HorizontalLine(x1, y1, x2, y2)
+    else DiagonalLine(x1, y1, x2, y2)
+}
+
+fun initMatrix(lines: List<Line>): Array<Array<Int>> {
+    val maxX = lines.maxOf { maxOf(it.startX, it.endX) }
+    val maxY = lines.maxOf { maxOf(it.startY, it.endY) }
+
+    return Array(maxY + 1) { Array(maxX + 1) { 0 } }
+}
+
+val lines = File("./05_input_02.txt")
+    .readLines()
+    .map { outer -> outer.split(" -> ").map { it.split(",").map(String::toInt) } }
+    .map { it.toLine() }
+
+val resultingMatrix = lines.fold(initMatrix(lines)) { matrix, line ->
+    matrix.apply {
+        line.construct().forEach { this[it.second][it.first] += 1 }
+    }
+}
+
+println(resultingMatrix.flatten().count { it > 1 })
