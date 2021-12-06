b16bf7388ad38e2b45eedbd3500068cfb3399a61
Christian Paling <christian.paling@googlemail.com>
2021-12-06 15:34:30 +0100

    Add day 06

---
+++ 06_day/06_day_01.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+def process_day(lanternfish)
+  new_fish = []
+
+  modified_lanternfish = lanternfish.map do |fish|
+    next fish - 1 unless fish.zero?
+
+    new_fish.push(8)
+    6
+  end
+
+  modified_lanternfish.concat(new_fish)
+end
+
+def process_days(initial_lanternfish, days)
+  (0...days).to_a.reduce(initial_lanternfish) { process_day(_1) }
+end
+
+puts File.read("./06_input_01.txt")
+       .strip
+       .split(",")
+       .map(&:to_i)
+       .then { |lanternfish| process_days(lanternfish, 80) }
+       .count
---
+++ 06_day/06_day_02.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+def process_day(lanternfish)
+  lanternfish.dup.tap do |l|
+    new_fish = l.shift
+
+    l.push(new_fish)
+    l[6] += new_fish
+  end
+end
+
+def process_days(initial_lanternfish, days)
+  (0...days).to_a.reduce(initial_lanternfish) { process_day(_1) }
+end
+
+def build_reduced_list(lanternfish)
+  lanternfish.each_with_object(9.times.map { 0 }) do |fish, reduced_list|
+    reduced_list[fish] += 1
+  end
+end
+
+puts File.read("./06_input_02.txt")
+       .strip
+       .split(",")
+       .map(&:to_i)
+       .then { |lanternfish| build_reduced_list(lanternfish) }
+       .then { |lanternfish_reduced| process_days(lanternfish_reduced, 256) }
+       .sum
