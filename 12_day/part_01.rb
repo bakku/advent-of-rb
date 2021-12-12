0ff25a49ba89523324a80566d3815ec5c6a4a4df
Christian Paling <christian.paling@googlemail.com>
2021-12-12 22:03:41 +0100

    Add day 12

---
+++ 12_day/part_01.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class Graph
+  def initialize(edges)
+    @vertices    = edges.flatten.uniq
+    @edges       = edges
+    @adjacencies = build_adjacency_hash
+  end
+
+  def find_paths
+    [].tap do |paths|
+      dfs("start", ["start"], paths)
+    end
+  end
+
+  def to_s
+    @adjacencies.to_s
+  end
+
+  private
+
+  def build_adjacency_hash
+    @vertices.map do |v|
+      adjacencies = @edges.select { |e| e.first == v || e.last == v }
+                      .map { (_1 - [v]).first }
+
+      [v, adjacencies]
+    end.to_h
+  end
+
+  def dfs(node, current_path, found_paths)
+    if node == "end"
+      found_paths << current_path
+      return
+    end
+
+    @adjacencies[node].each do |adjacency|
+      next if adjacency.downcase == adjacency && current_path.include?(adjacency)
+
+      dfs(adjacency, current_path + [adjacency], found_paths)
+    end
+  end
+end
+
+edges = File.readlines("./input.txt", chomp: true)
+  .map { _1.split("-") }
+
+p Graph.new(edges)
+    .find_paths
+    .count
